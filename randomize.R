6e14c99a18b9141b46ccae9207202d076df50dd6
hfisser <henrik.fisser@t-online.de>
2018-10-28 14:50:39 +0100

    -

---
+++ randomize.R
@@ -0,0 +1,4 @@
+# vector that contains all R course members
+members <- c('Basil', 'Belén', 'Camilo', 'Frederik', 'Flo', 'Giovanni', 'Haiyin', 'Jakob', 'Johannes', 'Magda', 'Malin', 'Martin', 'Michael', 'Nestor', 'Patrick', 'Ronja', 'Sandro', 'Tawhid', 'Tobias', 'Yumna')
+# random selection of one name from vector members
+sample(members, 1)
