4929d441b60b18447fc8959add1f026cd5156375
hfisser <henrik.fisser@t-online.de>
2018-10-30 12:19:51 +0100

    -

---
+++ indexing_matrix.R
@@ -0,0 +1,78 @@
+# indexing
+
+# change values of the data
+x <- c(1,2,3,4,5,6,10,45,22,55,60,32,49,1239,11,19,193,19,5,7,3,24,5,6,77,4,2,46,54)
+
+x <- numeric(length(x))
+x[x<=30] <- 1
+x[(x > 30) & (x <70) ] <- 2
+x[x > 70] <- 3
+x
+
+summary(x)
+sum(x)
+cumsum(x)
+
+rev(x)
+sort(x, decreasing = TRUE)
+sample(x,10)
+
+# matrix
+
+m1 <- matrix(c(4,6,7,3,4,9), nrow=2)
+m1
+m2 <- matrix(
+  c(2,3,5,7,2,4),
+  nrow=2,
+  ncol=3,
+  byrow=TRUE)
+m2
+
+m1[,2] # query 2 column
+m1[2,] # query 2 row
+m1[2,2] # query 2 row and 2 column
+
+# create a vector with 80 entries based on normally distributed data
+numbers_1 <- rnorm(80,mean=0,sd=1)
+mat_1 <- matrix(numbers_1,nrow=20,ncol=4)
+mat_1
+
+# matrix values into a data frame
+# the advantage is that we can use names and not only positions
+df_1 <- data.frame(mat_1)
+# assign column names
+names(df_1) <- c("pH","sal","tur","den")
+
+# dataframes
+df_1 <- data.frame(plot="location_name_1",measure1=runif(100)*1000,measure2=round(runif(100)*100),
+                   value=rnorm(100,2,1),ID=rep(LETTERS,100))
+df_2 <- data.frame(plot="location_name_2",measure1=runif(50)*100,measure2=round(runif(50)*10),
+                   value=rnorm(50),ID=rep(LETTERS,50))
+df <- rbind(df_1,df_2)
+df_1
+
+# merging two dataframes
+df <- rbind(df_1,df_2)
+
+# basic statistics
+summary(df)
+str(df)
+mode(df)
+head(df)
+
+# indexing a dataframe
+df[,c('plot','measure1','measure2')]
+
+# list from two vectors of different size
+a <- runif(199)
+b <- c("aa","bb","cc","dd","ee")
+c <- list(a,b)
+
+# index the second object
+c[2]
+# index first entry of second object
+c[[2]][1]
+c
+
+
+
