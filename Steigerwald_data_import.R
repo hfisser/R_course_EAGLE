4d5c3efe958935fe8e046f9d49733f4e68520d5f
hfisser <henrik.fisser@t-online.de>
2018-10-30 12:59:59 +0100

    -

---
+++ Steigerwald_data_import.R
@@ -0,0 +1,35 @@
+install.packages("RCurl")
+
+library(RCurl)
+
+df <- read.csv("https://raw.githubusercontent.com/wegmann/R_data/master/Steigerwald_sample_points_all_data_subset_withNames.csv")
+
+install.packages(tidyverse)
+
+library(tidyverse)
+
+head(df)
+
+tail(df)
+
+summary(df)
+
+plot(df)
+
+str(df)
+
+names(df)
+
+dim(df)
+
+class(df)
+
+levels(df)
+
+lucas <- df$LUCAS_LC[]
+
+ndvi <- df$L7.ndvi
+
+frame()
+
+plot(lucas,ndvi)
